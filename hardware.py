e0210e9c35b4113f7b913293eb0daa5458e9bd03
M-zudin <mikzudin@gmail.com>
2025-05-03 21:28:51 +0300

    Add files via upload

--- hardware.py
+++ hardware.py
@@ -6,7 +6,8 @@ import wmi
 import hashlib
 
 disk=input("Input drive letter: ")+':'
-if os.path.exists(disk):
+reserve_disk=input("Input reserve drive letter (optional): ")+':'
+if os.path.exists(disk) and os.path.exists(reserve_disk) and disk!=reserve_disk:
     input("WARNING!!! ALL data on disk will be lost. Press enter, if you are sure. Or close the program immediatly.")
     print("Cleaning disk...")
     for x in os.listdir(disk):
@@ -36,10 +37,14 @@ if os.path.exists(disk):
             main_disk_id=x.VolumeSerialNumber
         if x.DeviceID==disk:
             disk_id=x.VolumeSerialNumber
+        if x.DeviceID==reserve_disk:
+            reserve_disk_id=x.VolumeSerialNumber
     key=hashlib.pbkdf2_hmac('sha256',disk_id.encode(),main_disk_id.encode(),2*10**6,32)
     key=str(key)
     buf=1024**3
     pyAesCrypt.encryptFile(f"{disk}\\key.key","key.key",key,buf)
+    if reserve_disk!=':':
+        shutil.copy(f"{disk}\\key.key","temp_key.key")
     os.remove(f"{disk}\\key.key")
     shutil.move("key.key",f"{disk}\\key.key")
     os.remove("key.key")
@@ -51,6 +56,14 @@ if os.path.exists(disk):
         passwd+=symbols[pos%64]
         pos>>=6
     print(passwd)
+    if os.path.exists("temp_key.key"):
+        key=hashlib.pbkdf2_hmac('sha256',reserve_disk_id.encode(),main_disk_id.encode(),2*10**6,32)
+        key=str(key)
+        print("Creating reserve key...")
+        pyAesCrypt.encryptFile("temp_key.key",f"{reserve_disk}\\key.key",key,buf)
+        os.remove("temp_key.key")
+        print("Done!")
 else:
     print("There is no such disk")
+key=os.urandom(16)
 input("Press enter to exit...")
